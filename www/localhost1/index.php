a02c34e93138557b9475676d2a295741f21394dd
matteo <mazefeng@banggood.com>
2020-01-15 15:01:38 +0800

    php

--- www/localhost1/index.php
+++ www/localhost1/index.php
@@ -1,2 +1,11 @@
 <?php
+printExtensions();
 phpinfo();
+function printExtensions()
+{
+    echo '<ol>';
+    foreach (get_loaded_extensions() as $i => $name) {
+        echo "<li>", $name, '=', phpversion($name), '</li>';
+    }
+    echo '</ol>';
+}
